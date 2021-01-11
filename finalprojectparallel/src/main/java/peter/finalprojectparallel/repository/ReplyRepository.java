5609be60f1e84cea85fa87f4c2276f1891aa34df
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-11 10:53:40 -0500

    Delete ReplyRepository.java

--- finalprojectparallel/src/main/java/peter/finalprojectparallel/repository/ReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package peter.finalprojectparallel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import peter.finalprojectparallel.model.Reply;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}

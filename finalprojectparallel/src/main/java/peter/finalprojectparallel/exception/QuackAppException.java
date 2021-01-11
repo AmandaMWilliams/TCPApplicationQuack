32d9096c217d3cbafc494352b9bc82da307abf9b
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-11 10:38:05 -0500

    Delete QuackAppException.java

--- finalprojectparallel/src/main/java/peter/finalprojectparallel/exception/QuackAppException.java
+++
@@ -1,7 +0,0 @@
-package peter.finalprojectparallel.exception;
-
-public class QuackAppException extends RuntimeException {
-    public QuackAppException(String message) {
-        super(message);
-    }
-}

ca0df753cc7681f3c7aa59ff02be6014e4ebc9ab
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-11 10:53:30 -0500

    Delete MessageRepository.java

--- finalprojectparallel/src/main/java/peter/finalprojectparallel/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package peter.finalprojectparallel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import peter.finalprojectparallel.model.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}

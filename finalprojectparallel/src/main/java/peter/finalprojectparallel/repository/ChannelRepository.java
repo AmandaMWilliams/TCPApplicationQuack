cd4827ca3a8fb58b1d2e16ee950d629748932c89
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-11 10:39:14 -0500

    Delete ChannelRepository.java

--- finalprojectparallel/src/main/java/peter/finalprojectparallel/repository/ChannelRepository.java
+++
@@ -1,7 +0,0 @@
-package peter.finalprojectparallel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import peter.finalprojectparallel.model.Channel;
-
-public interface ChannelRepository extends JpaRepository<Channel, Long> {
-}

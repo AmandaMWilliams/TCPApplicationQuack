ca51ff9c5ca3d02a87cf824738c8d8f9facd801b
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2021-01-11 10:32:22 -0500

    Add files via upload

---
+++ finalprojectparallel/src/main/java/peter/finalprojectparallel/model/User.java
@@ -0,0 +1,35 @@
+package peter.finalprojectparallel.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotEmpty
+    @Email
+    private String email;
+    private Instant created;
+    private Boolean enabled;
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Channel> subscribedChannels;
+}

d6956ee93d9b371eed930c0da733f44f4cb3a3b0
AmandaMWilliams <amandawilliams1915@gmail.com>
2021-01-21 20:39:31 -0500

    Completed ChannelDTOTests

--- src/test/java/dtoTest/ChannelDtoTest.java
+++ src/test/java/dtoTest/ChannelDtoTest.java
@@ -33,4 +33,64 @@ public class ChannelDtoTest {
         //then
         Assertions.assertNotNull(channelDto);
     }
+
+    @Test
+    public void getChannelIDTest(){
+        //given
+        ChannelDto test = new ChannelDto();
+        Long expected = 8L;
+        //when
+        test.setChannelId(expected);
+        Long actual = test.getChannelId();
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getChannelNameTest(){
+        //given
+        ChannelDto test = new ChannelDto();
+        String expected = "Test Channel Name";
+        //when
+        test.setChannelName(expected);
+        String actual = test.getChannelName();
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDescriptionTest(){
+        //given
+        ChannelDto test = new ChannelDto();
+        String expected = "Test description";
+        //when
+        test.setDescription(expected);
+        String actual = test.getDescription();
+        //then
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getNumberOfMessagesTest(){
+        //given
+        ChannelDto test = new ChannelDto();
+        Integer expected = 5;
+        //when
+        test.setNumberOfMessages(expected);
+        Integer actual = test.getNumberOfMessages();
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOfUsersTest(){
+        //given
+        ChannelDto test = new ChannelDto();
+        Integer expected = 3;
+        //when
+        test.setNumberOfUsers(expected);
+        Integer actual = test.getNumberOfUsers();
+        //then
+        Assertions.assertEquals(expected, actual);
+    }
 }

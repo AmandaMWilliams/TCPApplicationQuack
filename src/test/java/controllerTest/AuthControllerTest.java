ea987ca63242f7db149a9dd62847866ac7913aab
Peter <pgkutc@gmail.com>
2021-01-21 20:57:41 -0500

    Controller Stubs

--- src/test/java/controllerTest/AuthControllerTest.java
+++ src/test/java/controllerTest/AuthControllerTest.java
@@ -3,22 +3,8 @@ package controllerTest;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import peter.finalprojectparallel.controller.AuthController;
-import peter.finalprojectparallel.controller.ChannelController;
-import peter.finalprojectparallel.dto.AuthenticationResponse;
-import peter.finalprojectparallel.dto.LoginRequest;
-import peter.finalprojectparallel.dto.RefreshTokenRequest;
-import peter.finalprojectparallel.dto.RegisterRequest;
 import peter.finalprojectparallel.service.AuthService;
-import peter.finalprojectparallel.service.ChannelService;
-
-import javax.validation.Valid;
 
 public class AuthControllerTest {
 
